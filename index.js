bf418802b4d2f65b4526d4e8867b188e036c4da6
Yedid Herskovitz <yedidmherskovitz@gmail.com>
2016-12-14 16:55:45 -0500

    isolate scope on each exported function call

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 "use strict";
 
-module.exports = () => {
-	let _fields = [];
-	let _maxDepth = 5;
+module.exports = function() {
+	return function() {
+		let _fields = [];
+		let _maxDepth = 5;
 
-	return {
-		fields(...args) {
-			_fields = _fields.concat(args);
-			return this;
-		},
+		return {
+			fields(...args) {
+				_fields = _fields.concat(args);
+				return this;
+			},
 
-		maxDepth(depth) {
-			if (depth) {
-				_maxDepth = depth;
-			}
-
-			return this;
-		},
-
-		mask(objIn) {
-			let objOut;
-
-			try {
-				objOut = JSON.parse(JSON.stringify(objIn));
-			} catch (e) {
-				objOut = Object.assign(objIn);
-			}
-
-			return function _mask(objOut, depth) {
-				depth = depth + 1;
-				if (typeof objOut !== 'object' || depth > _maxDepth) {
-					return objOut;
+			maxDepth(depth) {
+				if (depth) {
+					_maxDepth = depth;
 				}
 
-				for (let field of Object.keys(objOut)) {
-					if (_fields.includes(field)) {
-						delete objOut[field];
-					} else if (typeof objOut[field] === 'object') {
-						_mask(objOut[field], depth);
+				return this;
+			},
+
+			mask(objIn) {
+				let objOut;
+
+				try {
+					objOut = JSON.parse(JSON.stringify(objIn));
+				} catch (e) {
+					objOut = Object.assign(objIn);
+				}
+
+				return function _mask(objOut, depth) {
+					depth = depth + 1;
+					if (typeof objOut !== 'object' || depth > _maxDepth) {
+						return objOut;
 					}
-				}
 
-				return objOut;
-			}(objOut, 0);
-		}
-	};
+					for (let field of Object.keys(objOut)) {
+						if (_fields.includes(field)) {
+							delete objOut[field];
+						} else if (typeof objOut[field] === 'object') {
+							_mask(objOut[field], depth);
+						}
+					}
+
+					return objOut;
+				}(objOut, 0);
+			}
+		};
+	}();
 };
