b07ee0251633842970dfa6f45edd8b646edc4ba0
Yedid Herskovitz <yedidmherskovitz@gmail.com>
2016-12-15 12:00:16 -0500

    tests

---
+++ test/masker.js
@@ -0,0 +1,15 @@
+let should = require('chai').should();
+
+let testData = {
+	highestOne: {
+		middleOne: "middleOneVal"
+	}
+};
+
+describe('masker', function () {
+	describe('mask', function () {
+		it('should mask the given fields', function () {
+
+		});
+	});
+});
