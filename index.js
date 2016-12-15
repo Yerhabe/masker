7b5f252f524f0912c29a0693796941ec088b2329
Yedid Herskovitz <yedidmherskovitz@gmail.com>
2016-12-15 10:05:50 -0500

    going back to using fields for now

--- index.js
+++ index.js
@@ -2,26 +2,12 @@
 
 module.exports = function() {
 	return function() {
-		let _only = [];
-		let _except = [];
+		let _fields = [];
 		let _maxDepth = 5;
 
 		return {
-			only(...args) {
-				if (_except.length > 0) {
-					throw new Error('Cannot set "only" when "allExcept" is set');
-				}
-
-				_only = _only.concat(args);
-				return this;
-			},
-
-			allExcept(...args) {
-				if (_only.length > 0) {
-					throw new Error('Cannot set "allExcept" when "only" is set');
-				}
-
-				_except = _except.concat(args);
+			fields(...args) {
+				_fields = _fields.concat(args);
 				return this;
 			},
 
@@ -33,10 +19,6 @@ module.exports = function() {
 			},
 
 			mask(objIn) {
-				if (_only.length === 0 && _except.length === 0) {
-					return objIn;
-				}
-
 				let objOut;
 
 				try {
@@ -45,8 +27,6 @@ module.exports = function() {
 					objOut = Object.assign(objIn);
 				}
 
-				let shouldDelete = field => _only.length > 0 ? _only.includes(field) : !_except.includes(field);
-
 				return function _mask(objOut, depth) {
 					depth = depth + 1;
 					if (typeof objOut !== 'object' || depth > _maxDepth) {
@@ -54,7 +34,7 @@ module.exports = function() {
 					}
 
 					for (let field of Object.keys(objOut)) {
-						if (shouldDelete(field)) {
+						if (_fields.includes(field)) {
 							delete objOut[field];
 						} else if (typeof objOut[field] === 'object') {
 							_mask(objOut[field], depth);
